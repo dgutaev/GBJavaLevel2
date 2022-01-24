1934f4833650830777a862dd22c560bf2aaec823
dgutaev <dmgutaev@gmail.com>
2022-01-24 17:09:08 +0300

    Lesson 6 v1

--- src/main/java/Lesson6/ChatServer.java
+++ src/main/java/Lesson6/ChatServer.java
@@ -1,4 +1,20 @@
 package Lesson6;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.ServerSocket;
+
 public class ChatServer {
+    public static void main(String[] args) {
+        new ChatServer().start();
+    }
+
+    private void start() {
+        Socket socket = null;
+        try(ServerSocket serverSocket = new ServerSocket(8100)){
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
