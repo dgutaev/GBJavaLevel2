23ea3f0596e2c4fc8d5e6d7365d5604b7e947b50
dgutaev <dmgutaev@gmail.com>
2022-01-28 15:13:04 +0300

    Lesson 6 final

--- src/main/java/Lesson6/ChatClient.java
+++ src/main/java/Lesson6/ChatClient.java
@@ -3,25 +3,17 @@ package Lesson6;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ChatServer {
+public class ChatClient {
     public static void main(String[] args) {
-        ServerSocket server = null;
         Socket socket = null;
         try {
-            server = new ServerSocket(8189);
-            System.out.println("Сервер запущен, ожидаем подключения...");
-
-            socket = server.accept();
-            System.out.println("Клиент подключился");
-
+            socket = new Socket("localhost", 8189);
             final Scanner in = new Scanner(socket.getInputStream());
             final PrintStream out = new PrintStream(socket.getOutputStream());
             final Scanner console = new Scanner(System.in);
-
             Thread inThread = new Thread(() -> {
                 try {
                     while (true) {
@@ -30,12 +22,12 @@ public class ChatServer {
                             break;
                         }
                         System.out.println("Пришло сообщение: " + msg);
-
                     }
                 } catch (Exception e) {
                 }
             });
             inThread.start();
+
             Thread outThread = new Thread(() -> {
                 try {
                     while (true) {
@@ -43,25 +35,22 @@ public class ChatServer {
                         out.println(msg);
                     }
                 } catch (Exception e) {
+                    e.printStackTrace();
                 }
             });
-            outThread.setDaemon(true);
             outThread.start();
             try {
                 inThread.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
                 socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                server.close();
+                System.exit(1);
             } catch (IOException e) {
                 e.printStackTrace();
             }
